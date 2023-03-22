2f6ec6dd77ba55fa47f6f9ec30d3116ada6e944d
JakariaJishan <jakariajishan987@gmail.com>
2023-03-22 21:49:32 +0600

    Add Footer and finish styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from "react";
+import styles from '../style/Footer.module.css';
 function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div>
+      <div className={styles.footer}>Anime Co. Copyright ©2023 | Anime Co. S.L. All rights reserved.</div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
