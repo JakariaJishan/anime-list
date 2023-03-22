309c8ce4d54b8adbd02b311b8a38c91ef849e2cb
JakariaJishan <jakariajishan987@gmail.com>
2023-03-22 13:40:00 +0600

    Add style to card items

--- src/components/Anime.js
+++ src/components/Anime.js
@@ -1,17 +1,26 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import arrow from "../images/right-arrow.png";
+import styles from "../style/Anime.module.css";
 
 function Anime({ anime }) {
   const { mal_id, title, episodes, images } = anime;
 
   return (
-    <NavLink to={`/episodes/${mal_id}`}className>
-      <img src={images.jpg.image_url}width='300px' height='400px'></img>
-      <h2>{title}</h2>
-      <h3>{episodes}</h3>
-
-      
-    </NavLink>
+    <>
+      <NavLink to={`/episodes/${mal_id}`}>
+        <div className={styles.rightArrow}>
+          <img src={arrow} alt="right arrow icon" />
+        </div>
+        <div className={styles.card}>
+          <img src={images.jpg.image_url} width="150px" height="200px"></img>
+          <div className={styles.cardTitle}>
+            <h3>{title}</h3>
+            <h5>Total Episodes : {episodes}</h5>
+          </div>
+        </div>
+      </NavLink>
+    </>
   );
 }
 
