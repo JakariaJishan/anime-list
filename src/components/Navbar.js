995f941b58cf4258405410a6ebf1f45d4f27a291
JakariaJishan <jakariajishan987@gmail.com>
2023-03-22 22:00:42 +0600

    Finish styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { NavLink, useParams } from "react-router-dom";
+import { NavLink, useLocation, useParams } from "react-router-dom";
 import gear from "../images/gear.png";
+import home from "../images/home.png";
 import leftArrow from "../images/left-arrow.png";
 import menu from "../images/menu.png";
 import mic from "../images/microphone-black-shape.png";
 import styles from "../style/Navbar.module.css";
 function Navbar() {
   const { id } = useParams();
+  const location = useLocation();
   const episodes = useSelector((state) => state.animeList.animeList?.data);
   const filteredArr = episodes
     ? episodes?.filter((episode) => episode.mal_id == id)
@@ -16,9 +18,14 @@ function Navbar() {
   return (
     <header className={styles.header}>
       <nav className={styles.nav}>
-        <NavLink to="/" className={styles.arrow}>
-          <img src={leftArrow} height="20px" width="20px" />
-        </NavLink>
+        {location.pathname === `/episodes/${id}` && (
+          <NavLink to="/" className={styles.arrow}>
+            <img src={leftArrow} height="20px" width="20px" />
+          </NavLink>
+        )}
+        {location.pathname === "/" && (
+          <img src={home} height="20px" width="20px" />
+        )}
 
         {filteredArr[0] ? (
           <NavLink to="#" className={styles.brand}>
